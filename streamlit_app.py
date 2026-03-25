eb1d26214868a8a32580d0d962829b0c26c2f840
chakri-007-ui <adapachakri07@gmail.com>
2026-03-25 15:36:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,13 @@ if submitted:
     st.success("Someone clicked the button.", icon="👉")
 
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/all")  
-# st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon") 
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+if ingredients_list:
+   ingredients_string = ''
+
+  for fruit_chosen in ingredients_list:
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
