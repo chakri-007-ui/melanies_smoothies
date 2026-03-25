f6efe346ec11b9cc02283bb3d8f87c0c87054821
chakri-007-ui <adapachakri07@gmail.com>
2026-03-25 09:40:42 +0530

    Create streamlit_app.py
    copied from my Custom Smoothies SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+ 
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie!")
+import streamlit as st
+
+title = st.text_input("Movie title", "Life of Brian")
+st.write("The current movie title is", title)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+ 
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe
+    ,max_selections=5
+)
+ 
+ 
+if ingredients_list:
+    ingredients_string = ''
+ 
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+
+submitted = st.button('Submit')
+ 
+if submitted:
+    st.success("Someone clicked the button.", icon="👉")
